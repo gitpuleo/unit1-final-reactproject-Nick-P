e3d0b6171100b5e639249a4a9abd31999dd26625
nfp <nfpuleo@gmail.com>
2025-07-24 20:47:42 -0500

    Found and fixed a typo

--- unit1-final-project/src/Pages/LandingPage.jsx
+++ unit1-final-project/src/Pages/LandingPage.jsx
@@ -49,15 +49,16 @@ function LandingPage() {
             <div>
             <header>
               <h1>Welcome to Dynamic CV!</h1>
+              <br />
             </header>
-            <h2>Dynamic CV is designed to rebalance a broken recruitment process, allowing candidates to supply their full CV, and obligating  </h2>
+            <h2>Dynamic CV is designed to rebalance a broken recruitment process, allowing candidates to supply their full CV, and obligating...  </h2>
             <br/>
             <h3> &larr; Choose on the side panel what information you would like to know about--in this case--me.</h3>
             <br/>
             <h3> &larr; Hit the 'Generate' button to render your selected elements.</h3>
             <br />
             <p>
-            Enjoy the concept? Navigate to the License page to download the application for free. Add your data in the CVData.js file in the utils folder to render your own CV. All objects are extensible. See the ReadMe for guidance on substituting your own information. Enjoy and be good.
+            Enjoy the concept? Navigate to the License page to download the application for free. Substitute your data in the CVData.js file in the utils folder to render your own CV. All objects are extensible. See the ReadMe for guidance on substituting your own information. Enjoy and be good.
             </p>
             </div>
             )}
